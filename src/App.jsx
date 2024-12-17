74c729a0f91b62dd106db3e4a652e3391fb2e620
Susianti Amelia <38308638+Susiantiamelia@users.noreply.github.com>
2024-12-17 21:05:35 +0700

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -14,7 +14,7 @@ function App() {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/news" element={<Compare />} />
-        <Route path="/compare" element={<Compare />} />
+        <Route path="/compare" element={<News />} />
       </Routes>
     </Router>
   );
